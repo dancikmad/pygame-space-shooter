250e0665f4e2b1c213a7d83296b050a82f8b90ed
Daniel Crigan <crgdaniel@yahoo.com>
2025-03-18 14:24:15 +0000

    feat: add clock for consistent 60 FPS

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ import constants as c
 def main():
     pygame.init()
     screen = pygame.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT))
-
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         screen.fill((0, 0, 0))
         pygame.display.flip()
-
+        update_time = clock.tick(60)
+        dt = update_time / 1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return

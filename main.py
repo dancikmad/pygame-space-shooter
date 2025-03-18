02553b2c6a36ad701a9d5db4f4748ee090de53b4
Daniel Crigan <crgdaniel@yahoo.com>
2025-03-18 14:02:52 +0000

    init commit project

---
+++ main.py
@@ -0,0 +1,12 @@
+import pygame
+import constants as c
+
+
+def main():
+    print("Starting Asteroids!")
+    print("Screen width: {0}".format(c.SCREEN_WIDTH))
+    print("Screen height: {0}".format(c.SCREEN_HEIGHT))
+
+
+if __name__ == "__main__":
+    main()

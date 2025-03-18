9cc93ca637bf82f0e720f7851fd7bf19009b2e4a
Daniel Crigan <crgdaniel@yahoo.com>
2025-03-18 14:18:37 +0000

    feat: implement game screen & game loop

--- main.py
+++ main.py
@@ -3,9 +3,16 @@ import constants as c
 
 
 def main():
-    print("Starting Asteroids!")
-    print("Screen width: {0}".format(c.SCREEN_WIDTH))
-    print("Screen height: {0}".format(c.SCREEN_HEIGHT))
+    pygame.init()
+    screen = pygame.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT))
+
+    while True:
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
 
 
 if __name__ == "__main__":
